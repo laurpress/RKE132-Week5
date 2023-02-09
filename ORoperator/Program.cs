7a1775f668eb9df88597b38058827933f6fd828f
Laur Press <lpress@tktk.ee>
2023-02-09 19:50:26 +0200

    weekFive

---
+++ ORoperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Math >= 90 OR chemistry >= 90 OR biology >= 90
+
+//Math && chemistry || math && biology || chemistry && biology
+
+int math, biology, chemisty;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemisty = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && chemisty >= 90 || (math >= 90 && biology >= 90) || (chemisty >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
